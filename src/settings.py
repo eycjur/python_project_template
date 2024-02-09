223f35a4faff0525c122ec1c4330e5ae0752db83
eycjur <eycjur@gmail.com>
2024-02-10 02:20:20 +0900

    test: 環境変数によるfailを修正

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,12 @@
+import os
+
 from environs import Env
 
-env = Env()
+IS_GITHUB_ACTIONS = "GITHUB_ACTIONS" in os.environ
+
+
+# HACK: CI環境の場合は環境変数がなくてもエラーとしないように、遅延バリデーションとする
+env = Env(eager=not IS_GITHUB_ACTIONS)
 
 APP_PORT = env.int("APP_PORT")
 LOGGER_CONFIG_FILE = env.str("LOGGER_CONFIG_FILE")
