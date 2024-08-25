2abfc12b9b7a321e78c463eef1bf161de242fa0e
eycjur <eycjur@gmail.com>
2024-08-25 10:59:40 +0900

    feat: fastapiのディレクトリ構造を修正

---
+++ src/presentation/fastapi/controller/register_controller.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from injector import inject
+from pydantic import BaseModel
+
+from src.di import ingector
+from src.domain.message.message import Message
+from src.usecase.register import RegisterUsecase
+
+router = APIRouter()
+
+
+class RegisterRequest(BaseModel):
+    text: str
+
+
+class RegisterResponse(BaseModel):
+    text: str
+
+
+class RegisterController:
+    @inject
+    def __init__(self, register_usecase: RegisterUsecase):
+        self._register_usecase = register_usecase
+
+    def execute(self, request: RegisterRequest) -> RegisterResponse:
+        result = self._register_usecase.execute(Message(request.text))
+        return RegisterResponse(text=result)
+
+
+@router.post("/register")
+def register(request: RegisterRequest) -> RegisterResponse:
+    controller = ingector.get(RegisterController)
+    return controller.execute(request)
