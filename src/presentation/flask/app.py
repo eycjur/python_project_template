bc6199a36cccb07b948376a5617b06a65eddb56d
eycjur <eycjur@gmail.com>
2024-10-15 00:17:57 +0900

    feat: ruffのルールを追加

--- src/presentation/flask/app.py
+++ src/presentation/flask/app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 app.register_blueprint(router)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=CONTAINER_PORT)  # nosec
+    app.run(debug=True, host="0.0.0.0", port=CONTAINER_PORT)  # nosec  # noqa: S104,S201
